6516529aea274f0a168b0e46983baa009f81f510
Richard Klancer <rpk@pobox.com>
2012-03-10 16:05:59 -0500

    Begin require-ifying modeler stuff

--- src/modeler.js
+++ src/modeler.js
@@ -1,45 +1,14 @@
-/*globals modeler:true, d3, arrays, molecules_coulomb, molecules_lennard_jones, benchmark */
-/*jslint onevar: true devel:true eqnull: true */
+/*globals window, d3, molecules_coulomb, molecules_lennard_jones, benchmark */
+/*jslint devel: true eqnull: true */
 
-// modeler.js
-//
+var arrays, math, modeler;
 
-modeler = {};
+arrays = require('../arrays/arrays');
+math   = require('../math/math');
+
+exports.modeler = modeler = {};
+
 modeler.VERSION = '0.1.0';
-
-modeler.math = modeler.math || {};
-
-// Simple (Box-Muller) univariate-normal random number generator.
-//
-// The 'science.js' library includes a Box-Muller implementation which is likely to be slower, especially in a
-// modern Javascript engine, because it uses a rejection method to pick the random point in the unit circle.
-// See discussion on pp. 1-3 of:
-// http://www.math.nyu.edu/faculty/goodman/teaching/MonteCarlo2005/notes/GaussianSampling.pdf
-//
-modeler.math.normal = (function() {
-  var next = null;
-
-  return function(mean, sd) {
-    if (mean == null) mean = 0;
-    if (sd == null)   sd = 1;
-
-    var r, ret, theta, u1, u2;
-
-    if (next) {
-      ret  = next;
-      next = null;
-      return ret;
-    }
-
-    u1    = Math.random();
-    u2    = Math.random();
-    theta = 2 * Math.PI * u1;
-    r     = Math.sqrt(-2 * Math.log(u2));
-
-    next = mean + sd * (r * Math.sin(theta));
-    return mean + sd * (r * Math.cos(theta));
-  };
-}());
 
 modeler.makeIntegrator = function(args) {
 
@@ -893,7 +862,7 @@ modeler.model = function() {
         rmin                   = options.rmin                   || 4.4,
         mol_rmin_radius_factor = options.mol_rmin_radius_factor || 0.38,
 
-        webgl = !!window.WebGLRenderingContext,
+        webgl      = !!window && !!window.WebGLRenderingContext,
         not_safari = benchmark.what_browser.browser !== 'Safari',
 
         // special-case: Typed arrays are faster almost everywhere
